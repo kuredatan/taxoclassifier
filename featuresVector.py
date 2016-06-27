180175c5e35f82d4f932c2038d223652c8725303
Clémence <reda.clmc@live.fr>
2016-06-27 13:31:46 +0200

    featuresVector done

--- featuresVector.py
+++ featuresVector.py
@@ -4,7 +4,7 @@ from parsingFasta import parseFasta
 #Creates for each patient the corresponding features vector, that is:
 #@name: name of sample/patient
 #for each metadatum, (metadatum,value for this patient in the data matrix)
-#a list of couples (node,ID of read such as node matches this read)
+#a list of couples (node,list of IDs of read such as node matches these read in this sample)
 #(this is more efficient than an array of all nodes)
 
 def getCorrespondingID(sequenceID,idSequences):
@@ -17,30 +17,58 @@ def getCorrespondingID(sequenceID,idSequences):
         raise ValueError
     return i
 
+#@matches is a list of (read ID,sequences ID matching this read) pairs
+#returns a list of ID reads that @sequenceID matches
+def getMatchingReads(sequenceID,matches):
+    readsMatching = []
+    for pair in matches:
+        if not len(pair) == 2:
+            print "\n/!\ ERROR: Incorrect matches formatting."
+            raise ValueError
+        readID = pair[0]
+        idMatching = pair[1]
+        n = len(idMatching)
+        i = 0
+        while i < n and not idMatching[i] == sequenceID:
+            i += 1
+        if not (i == n):
+            readsMatching.append(readID)
+    return readsMatching
+        
 #Returns a list of features vectors
 def featuresCreate(sampleInfoList,infoList,filenames=["BC_M0_good","DC_M0_good","GC_M0_good","TR_M0_good","BC_M3_good","DC_M3_good","GC_M3_good","TR_M3_good","BJ_M0_good","EY_M0_good","GM_M0_good","BJ_M3_good","EY_M3_good","GM_M3_good"],fastaFileName="GREENGENES_gg16S_unaligned_10022015"):
     featuresVectorList = []
     idPatients,allMatches = parseAllMatch(filenames)
+    print "done"
     idSequences,phyloSequences = parseFasta(FastaFileName)
+    print "done"
     #Link between file name and sample name?
-    #As it is for now unknown, @featuresCreate returns the features vector list @featuresVectorList
+    #As it is for now unknown, @featuresCreate returns the features vector list @featuresVectorList (name of sample,metadataList) list
     #+ the list of couples (node,ID reads) apart @matchingSequences
     for sample in sampleInfoList:
+        print sample[0]
         n = len(sample)
         if n < 1:
             print "\n/!\ ERROR: Sample info incorrect."
             raise ValueError
-        #@name of sample
-        featuresVector = [sample[0]]
         metadataList = []
         for i in range(1,n):
             metadataList.append((infoList[i],sample[i]))
-        featuresVector += metadataList
-        featuresVectorList.append(featuresVector)
+        featuresVectorList.append((sample[0],metadataList))
     n = len(allMatches)
     #For each patient @idPatient[i]
     matchingSequences = []
     for i in range(n):
-        matching = [idPatient[i]]
-        
-    return featuresVectorList
+        print idPatient[i]
+        matchingPatient = []
+        for sequenceID in idSequences:
+            readsMatching = getMatchingReads(sequenceID,allMatches[i])
+            matchingPatient.append((sequenceID,readsMatching))
+        matchingSequences.append((idPatient[i],matchingPatient))
+    return featuresVectorList,matchingSequences
+
+def test():
+    from parsingInfo import parseInfo
+    sampleInfoList,infoList = parseInfo("Info")
+    featuresVectorList,matchingSequences = featuresCreate(sampleInfoList,infoList)
+    return featuresVectorList[:3],matchingSequences[:3]
